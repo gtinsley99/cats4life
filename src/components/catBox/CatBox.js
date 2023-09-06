b0ee0be523371c43ca92a5b9d031747448830d07
George Tinsley <george8s@live.co.uk>
2023-09-06 12:22:44 +0100

    added Catbox img ect

--- src/components/catBox/CatBox.js
+++ src/components/catBox/CatBox.js
@@ -1,5 +1,20 @@
-const CatBox = () => {
+import {faker} from "@faker-js/faker";
 
-};
+const CatBox = (props) => {
+    return (
+      <div>
+          {props.cats.map((item, index) => {
+            return (
+              <div key={index} value={index}>
+                <p>Name: {faker.person.firstName()}</p>
+                <p>Price: {faker.commerce.price()} </p>
+                <p>Location: {faker.location.country()}</p>
+                    <img src={item.url} width="500px" height="300px"/>
+              </div>
+            );
+          })}
+      </div>
+    );
+  };
 
 export default CatBox;
