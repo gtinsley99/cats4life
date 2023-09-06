d44ee853272727f2d77e0009b0ba212f02962ba9
George Tinsley <george8s@live.co.uk>
2023-09-06 13:24:34 +0100

    style boxes

--- src/components/catBox/CatBox.js
+++ src/components/catBox/CatBox.js
@@ -1,19 +1,20 @@
 import {faker} from "@faker-js/faker";
+import {Wrapper, Content} from "./CatBox.styles";
 
 const CatBox = (props) => {
     return (
-      <div>
+      <Wrapper>
           {props.cats.map((item, index) => {
             return (
-              <div key={index} value={index}>
+              <Content key={index} value={index}>
                 <p>Name: {faker.person.firstName()}</p>
                 <p>Price: £{faker.commerce.price({min: 20, max: 30})} </p>
                 <p>Location: {faker.location.country()}</p>
                     <img src={item.url} width="500px" height="300px"/>
-              </div>
+              </Content>
             );
           })}
-      </div>
+      </Wrapper>
     );
   };
 
---
+++ src/components/catBox/CatBox.styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content:space-evenly;
+    width: 90vw;
+    border: 3px solid black;
+    margin: 20px;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 3px solid black;
+    margin: 20px;
+`;
