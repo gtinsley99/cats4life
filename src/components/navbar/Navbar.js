9585b3e1074dbaab0940eacc44b93ea5876b03f7
Margaret Irungu <mwirungu22@gmail.com>
2023-09-07 13:19:03 +0100

    added titles and nav bar icons and somecss

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { AiTwotoneHome } from "react-icons/ai";
+import {BiSolidContact} from "react-icons/bi";
+import {SlBasketLoaded} from "react-icons/sl";
+
 const Navbar = () => {
 
-};
-
-export default Navbar;
+    return (
+      <div className="navBar">
+        <AiTwotoneHome />
+        <BiSolidContact />
+        <SlBasketLoaded className='basket' />
+      </div>
+    );
+  };
+  
+  export default Navbar;
--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,5 +1,16 @@
-const Title = () => {
+import React from 'react';
+import {BiSolidCat} from "react-icons/bi";
+import {FaCat} from "react-icons/fa";
 
-};
+
+export const Title = () => {
+  return (
+    <div className='titles'>
+        <h1 className='h1'> <BiSolidCat /> 4 LYFE</h1>
+        <h2 className='h2'>Give <FaCat/> a happy home today! </h2>
+        <h3 className='h3'>HAPPY SCROLLING!</h3>
+    </div>
+  )
+}
 
 export default Title;
