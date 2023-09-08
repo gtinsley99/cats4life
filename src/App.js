5ab8289ec23a7c7356d1e2bb8850f8fe12ce7b49
Zaid <zedpx101@outlook.com>
2023-09-08 15:18:55 +0100

    favourites code

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Catsapi from "./components/catsapi/Catsapi";
 import FakerPrice from "./components/fakerData/FakerPrice";
 import FakerLocation from "./components/fakerData/FakerLocation";
 import FakerName from "./components/fakerData/FakerName";
+import FavCats from "./components/favCats/FavCats";
 
 function App() {
   const [cats, setCats] = useState([]);
@@ -20,6 +21,7 @@ function App() {
   const [location, setLocation] = useState([]);
   const [name, setName] = useState([]);
   const [basketNumber, setBasketNumber] = useState(0);
+  const [images, setImages] = ([]);
 
   FakerPrice(setPrices);
   FakerLocation(setLocation);
@@ -30,6 +32,12 @@ function App() {
     <div className="App">
       <Navbar setShow={setShow} basketNumber={basketNumber}/>
       <Title />
+      <FavCats 
+        cats={cats}
+        images={images}
+        setImages={setImages}
+        name={name}
+      />
       <CatBox
         cats={cats}
         basket={basket}
