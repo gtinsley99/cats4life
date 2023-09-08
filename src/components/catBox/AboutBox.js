3b3e6744c16b89b19ac738dfebabc3de7367b65d
Jett Jagger <jettrjagger@hotmail.com>
2023-09-08 13:36:03 +0100

    about2

--- src/components/catBox/AboutBox.js
+++ src/components/catBox/AboutBox.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-const AboutBox = (cats, index) => {
-  console.log(cats[index]);
+const AboutBox = (props) => {
+  console.log(props)
+
   return (
     <div>
-      <h2>About (name)</h2>
-      <p>Description: {cats.breeds[0].description}</p>
-      <p>Age: {cats.breeds[0].age} years</p>
-      <p>Weight: {cats.breeds[0].weight} kg</p>
+      <h2>About {props.about}</h2>
+      <p>Description: {props.desc}</p>
+      <p>Age: {props.age} years</p>
+      <p>Weight: {props.weight.metric} kg</p>
       {/* <button onClick={onClose}>Close</button> */}
     </div>
   );
 };
-
-
 
 export default AboutBox;
--- src/components/catBox/CatBox.js
+++ src/components/catBox/CatBox.js
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
+
 import { faker } from "@faker-js/faker";
+
 import { Wrapper, Content, Button, Para, Image } from "./CatBox.styles";
+
 import AboutBox from "./AboutBox";
 
-const CatBox = (props) => {
-  const [showAbout, setShowAbout] = useState(false);
 
+
+const CatBox = (props) => {
+  const [showAbout, setShowAbout] = useState(Array(props.cats.length).fill(false));
   const addBasket = () => {
     props.setTotalPrice(props.totalPrice + 5);
     props.setShow(true);
+
   };
 
-  const openAboutMe = () => {
-    setShowAbout(true);
+  const openAboutMe = (index) => {
+    const updatedShowAbout = [...showAbout];
+    updatedShowAbout[index] = true;
+    setShowAbout(updatedShowAbout);
   };
 
-  const handleCloseAbout = () => {
-    setShowAbout(false);
+  const handleCloseAbout = (index) => {
+    const updatedShowAbout = [...showAbout];
+    updatedShowAbout[index] = false;
+    setShowAbout(updatedShowAbout);
   };
 
   return (
@@ -31,7 +40,31 @@ const CatBox = (props) => {
             <Image src={item.url} />
             <Button onClick={() => openAboutMe(index)}>About Me</Button>
             <Button onClick={addBasket}>Add to Basket</Button>
-            {showAbout ? <AboutBox cats={props.cats} index={index}/> : ""}
+            {showAbout[index] ? (
+
+<div>
+
+  <AboutBox
+
+    about={item.breeds[0].name}
+
+    desc={item.breeds[0].description}
+
+    age={item.breeds[0].age}
+
+    weight={item.breeds[0].weight}
+
+  />
+
+  <Button onClick={() => handleCloseAbout(index)}>Close</Button>
+
+</div>
+
+) : (
+
+""
+
+)}
           </Content>
         );
       })}
